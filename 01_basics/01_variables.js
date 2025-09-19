5578db35b0bfb62df2082b6ba96b8434f1630eb0
Pratik Timalsina <155095144+pratiktim7@users.noreply.github.com>
2025-09-19 15:05:12 +0000

    helo

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "pratiktimalsina@gmail.com"
+var accountPassword = "12345"
+accountCity = "Panauti"
+
+// accountId = 2 not allowed 
+accountEmail = "hchc@gmail.com"
+accountPassword ="212121"
+accountCity = "Bengaluru"
+
+console.table([accountEmail, accountId, accountPassword, accountCity]);
+
